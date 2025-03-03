8b6c3ed12f3969d3142bd5437b5a78b04030b220
Nadeeka Dilrukshi <ndilrukshi7394@gmail.com>
2025-03-03 12:47:51 +0800

    updated front end-backend files

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const bodyParser = require  ("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/api/dbinitialize", async function (req, res) {
+app.get("/dbinitialize", async function (req, res) {
   console.log("DB is getting initialized");
   let data = await dbinitialize();
 
