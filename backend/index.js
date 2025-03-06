5ffe28f99a76dcdd9c7d61f00c6997948106dbfc
Nadeeka Dilrukshi <ndilrukshi7394@gmail.com>
2025-03-06 12:37:34 +0800

    newly added changes

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ if(process.env.NODE_ENV === "test"){
     );
   });
 }else{
-  server.listen(4200, () => {
+  server.listen(8080, () => {
     console.log(
-      "Capstone Project Backend is running on http://localhost:4200"
+      "Capstone Project Backend is running on http://localhost:8080"
     );
   });
 }
